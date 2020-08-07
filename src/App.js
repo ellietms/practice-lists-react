6f94fc330ad625323e031783a7352415b67e405c
ellie <ellie.tms1371@gmail.com>
2020-08-07 22:46:45 +0100

    more neatly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, { useState } from 'react';
-import Validation from './Validation';
+import React, { useState } from "react";
+import Validation from "./Validation";
 // import CharComponent from './CharComponent';
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [input,setInput] = useState();
-  const [length,setLength] = useState();
+  const [input, setInput] = useState();
+  const [length, setLength] = useState();
   const handleInputLength = (event) => {
-    setLength(event.target.value.length)
-  }
+    setLength(event.target.value.length);
+  };
   const handleInputValue = (event) => {
     setInput(event.target.value);
-  }
+  };
   console.log(length);
   return (
     <div className="App">
-      <input type="text" onChange={(event) => {handleInputLength(event);
-      handleInputValue(event);}} value={input}/>
+      <input
+        type="text"
+        onChange={(event) => {
+          handleInputLength(event);
+          handleInputValue(event);
+        }}
+        value={input}
+      />
       <p> {input} </p>
       <p>The length is : {length}</p>
-      <Validation txtLength={length}/> 
+      <Validation txtLength={length} />
     </div>
   );
 }
--- src/Validation.js
+++ src/Validation.js
@@ -1,25 +1,14 @@
-import React from 'react';
+import React from "react";
 
-
-const Validation = ({txtLength}) => {
-    let length = txtLength;
-    let mainContent;
-    if(length > 5){
-        mainContent =(
-     <p>Text Too long</p>
-        )
-    }
-    else{
-    mainContent = (
-        <p>Text Too short</p>
-    )
-    }
-    return(
-        <div>
-               {mainContent}
-        </div>
-    )
-}
-
+const Validation = ({ txtLength }) => {
+  let length = txtLength;
+  let mainContent;
+  if (length > 5) {
+    mainContent = <p>Text Too long</p>;
+  } else {
+    mainContent = <p>Text Too short</p>;
+  }
+  return <div>{mainContent}</div>;
+};
 
 export default Validation;
